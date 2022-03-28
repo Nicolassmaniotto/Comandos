ed1791ff3e9eaf8226cdfe52be9a04d2240fc25d
Nicolas Smaniotto <nicolasantoniosmaniotto@gmail.com>
2022-03-28 09:18:35 -0300

    coreeçoes

---
+++ deb_manjaro.sh
@@ -0,0 +1,38 @@
+function getNome(){
+    IN="$1"
+    arrIN=(${IN//\// })
+    tamanho=${#arrIN[@]}
+    echo ${arrIN[$tamanho-1]} 
+}
+
+function getPath(){
+    IN="$1"
+    arrIN=(${IN//\// })
+    tamanho=${#IN}
+    tamanho2=${#arrIN[@]}
+    palavra=$arrIN[$tamanho2-1]
+    tamanho3=${#palavra}
+    calculo=$((tamanho2 - 1))
+    i=0;
+    for item in ${arrIN[@]}
+    do
+        if [ $i -lt $calculo ]; then
+            concac+="/"$item
+        fi
+        i=$((i+1))
+    done
+    echo $concac
+    # echo $IN | cut -b 
+}
+function installDeb(){
+    nome=$(getNome $1)
+    pasta=$(getPath $1)
+    tamanho=${#nome}
+    tamanho2=${#arrIN[@]}
+    palavra=$arrIN[$tamanho2-1]
+    tipo = ".deb"
+    if [palavra -eq  ]; then
+    echo $tipo;
+}
+
+installDeb $1
